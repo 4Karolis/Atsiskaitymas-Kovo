def0def7999af96d8aa225bc9a2f5a06493998da
4Karolis <4karolis.t@gmail.com>
2022-03-28 20:22:48 +0300

    Update Program.cs

--- Atsiskaitymas Kovo men/Program.cs
+++ Atsiskaitymas Kovo men/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Atsiskaitymas_Kovo_men_
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {           
+            List<int> BilietaiPo10Eur = new List<int>();
+            List<int> BilietaiPo20Eur = new List<int>();
+            List<int> BilietaiPo30Eur = new List<int>();
 
-            //List<int> SukurtiBilietaiPo10Eur
             Console.WriteLine("----------------------------------< MENIU >----------------------------------");
             Console.WriteLine("[1] PIRKTI bilietus, [2] KURTI bilietus. Q - iseiti ir programos.");
             int pirmasPasirinkimas = int.Parse(Console.ReadLine());
@@ -15,30 +18,36 @@ namespace Atsiskaitymas_Kovo_men_
 
             //bool pirmasTeisingasPasirinkimas = (pirmasPasirinkimas == "1" || "2" || "Q" || "q");
             //int pirmasPasirinkimasInt = int.Parse(pirmasPasirinkimas());
-            //bool notQ1 = (pirmasPasirinkimas != "q" || "Q");
+            //bool notQ1 = pirmasPasirinkimas != "q" || "Q";
 
             switch (pirmasPasirinkimas)
             {
-                case 1: Console.WriteLine("Pasirinkite bilieto tipa: [1] Po 10 €, [2] Po 20 €, [3] Po 30 €. Q - iseiti is programos.");
+                case 1: Console.WriteLine("Pasirinkite bilieto tipa: [1] Po 10 Eur, [2] Po 20 Eur, [3] Po 30 Eur. Q - iseiti is programos.");
                     int pirktiPasirinkimas = int.Parse(Console.ReadLine());
-                    break;
-                    switch (pirktiPasirinkimas)
-                    {
-                        
-                        case 1: Console.WriteLine("Iveskite kieki: ");
-                            int kiekis1 = int.Parse(Console.ReadLine());
-                            break;
-                        case 2: Console.WriteLine("Iveskite kieki: ");
-                            int kiekis2 = int.Parse(Console.ReadLine());
-                            break;
-                        case 3: Console.WriteLine("Iveskite kieki: ");
-                            int kiekis3 = int.Parse(Console.ReadLine());
-                            break;
-                        default:
-                            Console.WriteLine("Kazka neteisingai ivedete. Bankykite dar karta.");
-                            break;
-                    }
-                case 2: Console.WriteLine("Pasirinkite bilieto tipa: [1] Po 10 €, [2] Po 20 €, [3] Po 30 €. Q - iseiti is programos.");
+                    break;                    
+                        switch (pirktiPasirinkimas)
+                        {
+                            case 1:
+                                Console.WriteLine("Iveskite kieki: ");
+                                int kiekis1 = int.Parse(Console.ReadLine());
+                            for (int i = 0; i < kiekis1; i++)
+                            {
+                                BilietaiPo10Eur.Add(1);
+                            }                                                                                         
+                                break;
+                            case 2:
+                                Console.WriteLine("Iveskite kieki: ");
+                                int kiekis2 = int.Parse(Console.ReadLine());
+                                break;
+                            case 3:
+                                Console.WriteLine("Iveskite kieki: ");
+                                int kiekis3 = int.Parse(Console.ReadLine());
+                                break;
+                            default:
+                                Console.WriteLine("Kazka neteisingai ivedete. Bankykite dar karta.");
+                                break;
+                        }                    
+                case 2: Console.WriteLine("Pasirinkite bilieto tipa: [1] Po 10 Eur, [2] Po 20 Eur, [3] Po 30 Eur. Q - iseiti is programos.");
                     int kurtiPasirinkimas = int.Parse(Console.ReadLine());
                     break;
                     switch (kurtiPasirinkimas)
@@ -62,7 +71,12 @@ namespace Atsiskaitymas_Kovo_men_
                 default: Console.WriteLine("Kazka neteisingai ivedete. Bankykite dar karta.");
                     break;
 
-            }   
+            }
+            //Console.WriteLine("Nupirkta:");
+            //foreach (var item in BilietaiPo10Eur)
+            //{
+            //    Console.WriteLine(item);
+            //}
         }
     }
 }
