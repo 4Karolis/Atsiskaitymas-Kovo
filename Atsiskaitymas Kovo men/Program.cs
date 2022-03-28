c939ef845b12e537c882118913c369b816f248b5
4Karolis <4karolis.t@gmail.com>
2022-03-28 23:59:09 +0300

    Update Program.cs

--- Atsiskaitymas Kovo men/Program.cs
+++ Atsiskaitymas Kovo men/Program.cs
@@ -12,16 +12,28 @@ namespace Atsiskaitymas_Kovo_men_
             List<int> BilietaiPo20Eur = new List<int>();
             List<int> BilietaiPo30Eur = new List<int>();
 
-            int bilietai10 = 0;
-            int bilietai20 = 0;
-            int bolietai30 = 0;
+            //int bilietai10 = 0;
+            //int bilietai20 = 0;
+            //int bolietai30 = 0;
 
             while (true)
-            //{
-            //    Console.WriteLine("----------------------------------< MENIU >----------------------------------");
-            //    Console.WriteLine("[1] PIRKTI bilietus, [2] KURTI bilietus.");
-            //    int pirktiKurti = int.Parse(Console.ReadLine());
-            
+            {
+                Console.WriteLine("----------------------------------< MENIU >----------------------------------");
+                Console.WriteLine("[1] PIRKTI bilietus, [2] KURTI bilietus, [3] ATASKAITA");
+                int pirktiKurti = int.Parse(Console.ReadLine());
+
+                int bilietuPo10Kiekis = BilietaiPo10Eur.Sum();
+                int bilietuPo20Kiekis = BilietaiPo20Eur.Sum();
+                int bilietuPo30Kiekis = BilietaiPo30Eur.Sum();
+
+                //int visoSukurta10 = 0;
+                //int visoSukurta20 = 0;
+                //int visoSukurta30 = 0;
+
+                //int visoParduota10 = 0;
+                //int visoParduota20 = 0;
+                //int visoPardduota30 = 0;
+
 
                 switch (pirktiKurti)
                 {
@@ -97,23 +109,28 @@ namespace Atsiskaitymas_Kovo_men_
                             Console.WriteLine("Neteisinga ivestis.Bandyk.");
                         }
                         break;
-
+                    case 3:
+                        Console.WriteLine($"Esamu bilietu kiekis: Po 10 Eur [{bilietuPo10Kiekis}], Po 20 Eur [{bilietuPo20Kiekis}], Po 30 Eur [{bilietuPo30Kiekis}]");
+                        break;                    
                 }
             }
 
+            int visoSukurta10 = bilietuKiekii.Count("1");
+           
+
             //panaudosiu veliau, paziuret ar nesu minuse su bilietais
             //arba geriau atskirus daryt pirkimo / pardavimo listus...
-            int rezultatasPo10 = BilietaiPo10Eur.Sum();
-            Console.WriteLine(rezultatasPo10);
+            //int rezultatasPo10 = BilietaiPo10Eur.Sum();
+            //Console.WriteLine(rezultatasPo10);
             //Console.WriteLine($"po 10 yra: {BilietaiPo10Eur.Count}, 20: {BilietaiPo20Eur.Count}, po 30: {BilietaiPo30Eur.Count}");
 
         }//---------------------------------------------------------------------------------------------------------------------------
-        public static void MainMenu()
-        {
-            Console.WriteLine("----------------------------------< MENIU >----------------------------------");
-            Console.WriteLine("[1] PIRKTI bilietus, [2] KURTI bilietus.");
-            int pirktiKurti = int.Parse(Console.ReadLine());
-            //return pirktiKurti;
-        }
+         //public static void MainMenu()
+         //{
+         //    Console.WriteLine("----------------------------------< MENIU >----------------------------------");
+         //    Console.WriteLine("[1] PIRKTI bilietus, [2] KURTI bilietus.");
+         //    int pirktiKurti = int.Parse(Console.ReadLine());
+         //    //return pirktiKurti;
+        //}
     }
 }
