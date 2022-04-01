008e090dd3278fbf4200c9b73c4b706d9c826474
4Karolis <4karolis.t@gmail.com>
2022-04-01 22:21:53 +0300

    Update Program.cs

--- Atsiskaitymas Kovo men/Program.cs
+++ Atsiskaitymas Kovo men/Program.cs
@@ -7,16 +7,20 @@ namespace Atsiskaitymas_Kovo_men_
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
+            #region LIST'ai         
             List<int> SukurtiPo10 = new List<int>();
             List<int> SukurtiPo20 = new List<int>();
             List<int> SukurtiPo30 = new List<int>();
 
             List<int> NupirktiPo10 = new List<int>();
             List<int> NupirktiPo20 = new List<int>();
-            List<int> NupirktiPo30 = new List<int>();            
+            List<int> NupirktiPo30 = new List<int>();
+            #endregion
 
-            while (true)
+            #region PROGRAMA
+            bool Dasviduli = false;
+            while (!Dasviduli)
             {                
                 int pirktiKurti = MainMenu();
                 switch (pirktiKurti)
@@ -25,7 +29,7 @@ namespace Atsiskaitymas_Kovo_men_
                         int bilietoTipasPirkti = Pirkti(pirktiKurti);
                         if (bilietoTipasPirkti == 1)
                         {                            
-                            IveskiteEuruKieki(bilietoTipasPirkti);
+                            IveskiteEuruKieki(bilietoTipasPirkti);                            
                             int kiekis = int.Parse(Console.ReadLine());
                             //GaunuKieki(bilietoTipasPirkti);//pabandyt gaut atskira metoda cw!
                             Add2(NupirktiPo10, kiekis);                            
@@ -77,21 +81,17 @@ namespace Atsiskaitymas_Kovo_men_
                         Ataskaita20(SukurtiPo20, NupirktiPo20);
                         Ataskaita30(SukurtiPo30, NupirktiPo30);
                         break;
-                }                
-            }
-
-        }//---------------------------------------------------------------------------------------------------------------------------
-        //public static PridetParduota10()
-        //{
-
-        //}
-        //public static int Kurti(int pirktiKurti)
-        //{
-        //    Console.WriteLine("-----------------------< Pasirinkite bilieto tipa >--------------------------");
-        //    Console.WriteLine("[1] Po 10 Eur, [2] Po 20 Eur, [3] Po 30 Eur");
-        //    int pasirinkimasKurti = int.Parse(Console.ReadLine());
-        //    return pasirinkimasKurti;
-        //}
+                    case 4:
+                        Dasviduli = false;
+                        break;
+                        Console.Clear();
+                        Console.WriteLine("Aciu, kad naudojates musu paslaugomis. Iki kito karto!");
+                }               
+            }        
+        }
+        #endregion
+        //---------------------------------------------------------------------------------------------------------------------------
+        #region METODAI
         public static void Ataskaita10(List<int> SukurtiPo10, List<int> NupirktiPo10)
         {
             Console.WriteLine($"Bilietai po 10 EUr: sukurta [{SukurtiPo10.Count}], parduota [{NupirktiPo10.Count}], likutis [{SukurtiPo10.Count - NupirktiPo10.Count}]");
@@ -101,39 +101,24 @@ namespace Atsiskaitymas_Kovo_men_
         {
             Console.WriteLine($"Bilietai po 20 EUr: sukurta [{SukurtiPo20.Count}], parduota [{NupirktiPo20.Count}], likutis [{SukurtiPo20.Count - NupirktiPo20.Count}]");
 
-        } public static void Ataskaita30(List<int> SukurtiPo30, List<int> NupirktiPo30)
+        } 
+        public static void Ataskaita30(List<int> SukurtiPo30, List<int> NupirktiPo30)
         {
             Console.WriteLine($"Bilietai po 10 EUr: sukurta [{SukurtiPo30.Count}], parduota [{NupirktiPo30.Count}], likutis [{SukurtiPo30.Count - NupirktiPo30.Count}]");
 
         }
-        public static int GaunuKieki(int pasirinkimasPirkti)
+        public static int GaunuKieki()
         {
             int kiekis = int.Parse(Console.ReadLine());
             return kiekis;
         }
         public static void Add2(List<int> intList, int kiekis)
-        {
-            //list to store elements to be added
-           
-            //int skaicius = int.Parse(Console.ReadLine());
+        {            
             for ( int c = 0; c < kiekis; c++)
             {
                 intList.Add(1);
-            }
-            //return c;
-            //ints.AddRange(inserts);
-        }
-        public static void Prideti(List<int> intList, int pasirinkimasPirkti, int j)
-        {
-            Console.WriteLine("-----------------------------< Iveskite kieki >------------------------------");
-            //int kiekis = int.Parse(Console.ReadLine());
-            //return kiekis;
-            for (int i = 0; i < j; i++)
-            {
-                
-            }
-           // Add2(List<> intList,)
-        }
+            }           
+        }       
         public static void AddToList(List<int> intList, int skaicius)// del , 1 gal
         {
             int a = 1;
@@ -162,9 +147,10 @@ namespace Atsiskaitymas_Kovo_men_
          public static int MainMenu()
         {
             Console.WriteLine("----------------------------------< MENIU >----------------------------------");
-            Console.WriteLine("[1] PARDUOTI bilietus, [2] KURTI bilietus, [3] ATASKAITA");
+            Console.WriteLine("[1] PARDUOTI bilietus, [2] KURTI bilietus, [3] ATASKAITA, [4] ISEITI is programos");
             int pirktiKurti = int.Parse(Console.ReadLine());
             return pirktiKurti;
         }
+        #endregion
     }
 }
